813d8e470cf9b2d437890797e81e80dfd7eefdd4
IsaacoHamsto <isaac.as@outlook.com>
2023-04-08 18:12:49 -0300

    Animar o logo no menu

--- HUD.gd
+++ HUD.gd
@@ -1,6 +1,10 @@
 extends CanvasLayer
 
 signal start_game
+var time = 0
+
+func _ready():
+	$MainMenu/Logo.pivot_offset = $MainMenu/Logo.size/2
 
 func _on_playButton_pressed():
 	$MainMenu.hide()
@@ -16,3 +20,14 @@ func _on_messageTimer_timeout():
 
 func show_game_over():
 	show_message("FIM DE JOGO")
+
+func logo_animate(t):
+	var logoScale = Vector2.ONE
+	logoScale=logoScale*(sin(t)/3+1)
+	$MainMenu/Logo.scale = logoScale
+
+func _process(delta):
+	time += delta
+	if(time>PI):
+		time -= PI
+	logo_animate(time)
