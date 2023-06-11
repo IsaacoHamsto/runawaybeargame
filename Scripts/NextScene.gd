afda17516afe88a4e72c033125b2ae431b3e7678
IsaacoHamsto <isaac.as@outlook.com>
2023-06-11 19:05:01 -0300

    Adicionar mais sons e música

--- Scripts/NextScene.gd
+++ Scripts/NextScene.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 func _on_area_entered(area):
-	owner.level += 1
 	owner.get_node("AnimationPlayer").play("fade_out")
 	await owner.get_node("AnimationPlayer").animation_finished
+	owner.level += 1
 	get_tree().change_scene_to_file("res://Scenes/Levels/Level" + str(owner.level) + ".tscn")
